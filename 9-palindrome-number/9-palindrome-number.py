c74effa129862610787272577b95ad7810798a56
Sachin dabas <77283739+sachin-dabas@users.noreply.github.com>
2022-03-27 10:05:41 -0400

    Time: 76 ms (69.88%), Space: 13.9 MB (65.27%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -1,5 +1,18 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        s = str(x)
-        rev = s[::-1]
-        return s == rev
+        i = 0
+        result = 0
+        s = x
+        while s > 0:
+            result = (result*10)
+            result += (s%10)
+            i += 1
+            s = s//10
+        return x == result
+
+
+
+    # def isPalindrome(self, x: int) -> bool:
+    #     s = str(x)
+    #     rev = s[::-1]
+    #     return s == rev
